e13271b0530884dac787026f14ffc64829ad4ffa
Arko Mustafi <64656617+Arkomustafi2@users.noreply.github.com>
2022-06-27 22:13:36 +0600

    Add files via upload

---
+++ A. Elephant.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+
+    int n,a,b;
+    cin>> n;
+    a= n/5;
+    if(n%5!=0){
+        a++;
+    }
+    cout<< a;
+
+}
