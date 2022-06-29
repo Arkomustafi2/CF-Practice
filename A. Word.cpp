a403567e0a249cceed1be3bed653df882a8a366d
Arko Mustafi <64656617+Arkomustafi2@users.noreply.github.com>
2022-06-30 00:20:17 +0600

    Update A. Word.cpp

--- A. Word.cpp
+++ A. Word.cpp
@@ -7,7 +7,26 @@
 using namespace std;
 int main()
 {
-
-
-
+    char s[108];
+    while(~scanf("%s",s))
+    {
+        int n=strlen(s),big=0,small=0;
+        for(int i=0; i<n; i++)
+            if(s[i]>=97)
+                small++;
+            else
+                big++;
+        if(small>=big)
+        {
+            for(int i=0; i<n; i++)
+                if(s[i]<97)
+                    s[i]+=32;
+        }
+        else
+            for(int i=0; i<n; i++)
+                if(s[i]>=97)
+                    s[i]-=32;
+        cout<<s<<endl;
+    }
+    return 0;
 }
